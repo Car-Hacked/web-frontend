89a5c3839d0eb908ac6a3217f46f67e1c3bd9fbf
timgass <timothyagass@gmail.com>
2020-01-26 09:12:13 -0600

    Initial dist subtree commit

---
+++ precache-manifest.5ab11da99d3addc55807cf6846408e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a567e1c1783ff222063a9db03387f1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36fb05b19cc3e69a9698",
+    "url": "/static/css/main.4c39a1d4.chunk.css"
+  },
+  {
+    "revision": "62a35d10f661376c5abf",
+    "url": "/static/js/2.61e08bee.chunk.js"
+  },
+  {
+    "revision": "9fbcf26faf345f33038f61e1827d66ff",
+    "url": "/static/js/2.61e08bee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36fb05b19cc3e69a9698",
+    "url": "/static/js/main.71b952c0.chunk.js"
+  },
+  {
+    "revision": "a175cdf622c534cd171d",
+    "url": "/static/js/runtime-main.030d1d5a.js"
+  },
+  {
+    "revision": "ff7abf12f0496ac5a9d38932c850caca",
+    "url": "/static/media/map.ff7abf12.png"
+  }
+]);
